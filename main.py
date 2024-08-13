d049cfc229ec19dec076acecebb46adaa444db28
Ahmed Hussein <wolktheline@yahoo.com>
2024-08-12 23:26:46 -0600

    Creating app for a company employees.

---
+++ main.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(layout="wide")
+col1, col2 = st.columns(2)
+
+
+content = """
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+"""
+
+st.header("The Best Company")
+st.write(content)
+
+st.subheader("Our Team")
+
+df = pd.read_csv("data.csv")
+
+col3, col4, col5 = st.columns([1.5, 1.5, 1.5])
+
+with col3:
+    for index, row in df[:4].iterrows():
+        st.subheader(f"{row['first name'].title()} {row['last name'].title()}")
+        st.write(row["role"])
+        st.image("images/" + row["image"])
+
+with col4:
+    for index, row in df[4:8].iterrows():
+        st.subheader(f"{row['first name'].title()} {row['last name'].title()}")
+        st.write(row["role"])
+        st.image("images/" + row["image"])
+
+with col5:
+    for index, row in df[8:].iterrows():
+        st.subheader(f"{row['first name'].title()} {row['last name'].title()}")
+        st.write(row["role"])
+        st.image("images/" + row["image"])
